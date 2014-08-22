a54bb899ce7fd2c55fa776fcb8d3c52494ab407d
David E Smith <dsmith@tgen.org>
2014-08-22 09:20:28 -0700

    0.9.7: Documentation, examples, partial merge of updates, removed some broken memory-optimizing code

--- vcf_to_matrix.py
+++ vcf_to_matrix.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 __author__ = "David Smith"
-__version__ = "0.9.6"
+__version__ = "0.9.7"
 __email__ = "dsmith@tgen.org"
 
 import logging
 
 
 def _parse_args():
+    """
+    Parse command line arguments and return an object whose attributes are the
+    settings provided, as per standard argparse behavior.
+    For backward compatibility when we were changing dispatchers, the options
+    could be directly provided on the command line, or a single argument could
+    specify the path to the XML file with the remaining arguments in it.  As
+    the old dispatcher is deprecated, so perhaps is every option below except
+    '--dto-file', which would then be made required.
+    """
     import argparse
     parser = argparse.ArgumentParser( description="Meant to be called from the pipeline automatically." )
     parser.add_argument( "--mode", required=True, choices=[ 'commandline', 'xml' ], help="Data passing mode, must be set to 'commandline' or 'xml'." )
@@ -26,6 +35,14 @@ def _parse_args():
     return parser.parse_args()
 
 def _parse_input_config(commandline_args):
+    """
+    Set the configuration options from the XML file and add them as attributes
+    to the object returned by _parse_args().
+    Since getting these options directly from the command line is perhaps
+    deprecated, we should probably store the configuration options from the
+    XML file in a separate dictionary, instead of mixing the two data sources
+    together as now.
+    """
     import matrix_DTO
     (matrix_parms, input_files) = matrix_DTO.parse_dto(commandline_args.dto_file)
     commandline_args.reference_fasta = matrix_parms['reference-fasta']
@@ -42,6 +59,11 @@ def _parse_input_config(commandline_args):
     return commandline_args
 
 def import_reference( reference, reference_path, dups_path ):
+    """
+    Take an empty reference object and populate it with the data from a
+    reference file, and a dups file if any.
+    Does not return anything, as the passed-in object is modified.
+    """
     reference.import_fasta_file( reference_path )
     if dups_path is not None:
         reference.import_dups_file( dups_path )
@@ -50,6 +72,12 @@ def import_reference( reference, reference_path, dups_path ):
     #reference._dups._send_to_fasta_handle( stdout )
 
 def import_external_fasta( input_file ):
+    """
+    Create a FastaGenome object, set its metadata, and populate it with the
+    data from a fasta file.
+    Must return the data as the single item in an array, because other file
+    formats potentially contain several genomes.
+    """
     from nasp_objects import FastaGenome
     genome = FastaGenome()
     set_genome_metadata( genome, input_file )
@@ -61,15 +89,16 @@ def import_external_fasta( input_file ):
 # FIXME split into a larger number of smaller more testable functions
 # FIXME This belongs in VCFGenome object perhaps?
 def read_vcf_file( reference, min_coverage, min_proportion, input_file ):
+    """
+    Submit VCF to be read in to VCF parser, populate genome data and filter
+    data from the parsed VCF data, return a list of the read-in genomes.
+    """
     genomes = {}
     file_path = get_file_path( input_file )
     with open( file_path, 'r' ) as vcf_filehandle:
         from nasp_objects import VCFGenome, Genome, ReferenceCallMismatch, VCFRecord
-        #import vcf
         vcf_record = VCFRecord( file_path )
-        #vcf_data_handle = vcf.Reader( vcf_filehandle )
         vcf_samples = vcf_record.get_samples()
-        #print( vcf_samples )
         for vcf_sample in vcf_samples:
             genomes[vcf_sample] = VCFGenome()
             set_genome_metadata( genomes[vcf_sample], input_file )
@@ -77,10 +106,12 @@ def read_vcf_file( reference, min_coverage, min_proportion, input_file ):
         while vcf_record.fetch_next_record():
             current_contig = vcf_record.get_contig()
             current_pos = vcf_record.get_position()
+            # Skip if position isn't in reference; maybe user truncated reference to exclude an uninteresting region.
             if current_pos <= reference.get_contig_length( current_contig ):
                 reference_call = reference.get_call( current_pos, None, current_contig )
                 simplified_refcall = Genome.simple_call( reference_call )
                 if ( simplified_refcall != 'N' ) and ( simplified_refcall != Genome.simple_call( vcf_record.get_reference_call()[0] ) ):
+                    # Reference call from reference fasta differs from reference call in VCF file at the same position.
                     raise ReferenceCallMismatch( reference_call, vcf_record.get_reference_call(), file_path, current_contig, current_pos )
                 for vcf_sample in vcf_samples:
                     sample_info = vcf_record.get_sample_info( vcf_sample )
@@ -100,16 +131,21 @@ def read_vcf_file( reference, min_coverage, min_proportion, input_file ):
                         else:
                             genomes[vcf_sample].set_proportion_pass( 'N', current_pos, current_contig )
                     elif not sample_info['is_a_snp']:
+                        # Some big SNP callers, like GATK, do not provide proportion information when
+                        # the position is called reference.  We cannot filter these positions.
                         genomes[vcf_sample].set_proportion_pass( '-', current_pos, current_contig )
     #from sys import stdout
     #for genome in genomes:
     #    genomes[genome]._genome._send_to_fasta_handle( stdout )
     return genomes.values()
 
-# FIXME These three functions should be combined?
+# FIXME The information these three functions capture is in the XML file.
+# FIXME These should all be deprecated and the XML data used instead.
 def determine_file_type( input_file ):
+    """ Get the file type from the packed input filename string """
     import re
     file_type = None
+    # Format is "type,aligner,caller,::path"
     filename_match = re.match( r'^((?:[^,:]+,)+)::.*$', input_file )
     if filename_match:
         generator_array = filename_match.group(1).split( ',' )
@@ -118,8 +154,10 @@ def determine_file_type( input_file ):
     return file_type
 
 def get_file_path( input_file ):
+    """ Get the file path from the packed input filename string """
     import re
     file_path = None
+    # Format is "type,aligner,caller,::path"
     filename_match = re.match( r'^(?:[^,:]+,)+::(.*)$', input_file )
     if filename_match:
         file_path = filename_match.group(1)
@@ -129,7 +167,9 @@ def get_file_path( input_file ):
     return file_path
 
 def set_genome_metadata( genome, input_file ):
+    """ Get all the metadata from the packed input filename string """
     import re
+    # Format is "type,aligner,caller,::path"
     filename_match = re.match( r'^((?:[^,:]+,)+)::(.*)$', input_file )
     if filename_match:
         generator_array = filename_match.group(1).split( ',' )
@@ -141,6 +181,14 @@ def set_genome_metadata( genome, input_file ):
     #print( genome.identifier() )
 
 def manage_input_thread( reference, min_coverage, min_proportion, input_q, output_q ):
+    """
+    Manage one input file worker thread, for reading the data from the file.
+    Input filenames are pulled one at a time from the input queue, and the
+    genome data from the read-in files is placed on the output queue.
+    When an input filename of "None" appears, we know we're done and put
+    "None" on the output queue so the controlling thread knows we won't be
+    adding more data.
+    """
     input_file = input_q.get()
     while input_file is not None:
         try:
@@ -155,49 +203,74 @@ def manage_input_thread( reference, min_coverage, min_proportion, input_q, outpu
         except:
             failed_file_path = get_file_path( input_file )
             logging.exception( "Unable to read in data from '{0}'!".format( failed_file_path ) )
+            # If the genome on the output queue is just a string, its contents
+            # are the name of the file we failed to read in.
             output_q.put( failed_file_path )
         input_file = input_q.get()
     output_q.put( None )
 
 def parse_input_files( input_files, num_threads, genomes, min_coverage, min_proportion ):
-    from multiprocessing import Process, Queue
-    #from queue import Queue
+    """
+    Use a pool of worker threads to, in parallel, read in the input files.
+    Populate the genome collection with the read-in data.
+    This is the "poison pill" thread management algorithm, where threads are
+    each given a "you can stop now" task once the actual queue of tasks is
+    complete.
+    """
+    # Lines below marked "Single-thread" can be uncommented and replace
+    # the lines marked "Multi-thread" to get single-thread behavior.
+    from multiprocessing import Process, Queue  # Multi-thread
+    #from queue import Queue  # Single-thread
     from time import sleep
     input_q = Queue()
     output_q = Queue()
     for input_file in input_files:
         input_q.put( input_file )
+    # If the number of jobs is already smaller than the thread pool...
     if num_threads > input_q.qsize():
         num_threads = input_q.qsize()
     sleep( 1 )
     thread_list = []
     for current_thread in range( num_threads ):
         input_q.put( None )
-        current_thread = Process( target=manage_input_thread, args=[ genomes.reference(), min_coverage, min_proportion, input_q, output_q ] )
-        current_thread.start()
-        #manage_input_thread( genomes.reference(), min_coverage, min_proportion, input_q, output_q )
-        thread_list.append( current_thread )
+        current_thread = Process( target=manage_input_thread, args=[ genomes.reference(), min_coverage, min_proportion, input_q, output_q ] )  # Multi-thread
+        current_thread.start()  # Multi-thread
+        #manage_input_thread( genomes.reference(), min_coverage, min_proportion, input_q, output_q )  # Single-thread
+        thread_list.append( current_thread )  # Multi-thread
     sleep( 1 )
     while num_threads > 0:
         new_genome = output_q.get()
         if new_genome is None:
             num_threads -= 1
         elif isinstance( new_genome, str ):
+            # Reading this file in failed.  We only know the filename.
             genomes.add_failed_genome( new_genome )
         else:
             genomes.add_genome( new_genome )
     sleep( 1 )
-    for current_thread in thread_list:
-        current_thread.join()
+    for current_thread in thread_list:  # Multi-thread
+        current_thread.join()  # Multi-thread
 
 def write_output_matrices( genomes, master_matrix, filter_matrix, matrix_format ):
+    """ Write matrices from genome collection data """
+    # FIXME Multiple matrix formats defined here
     genomes.write_to_matrices( master_matrix, filter_matrix, matrix_format )
 
 def write_stats_data( genomes, general_stats, sample_stats ):
+    """ Write stats data from genome collection to passed-in filenames """
     genomes.write_to_stats_files( general_stats, sample_stats )
 
 
 def main():
+    """
+    Main flow control function for the script.  Is sequential over the
+    following steps:
+    1.  Parse arguments
+    2.  Read in reference
+    3.  Read in all query genomes in parallel
+    4.  Write output matrices
+    5.  Write stats files
+    """
     commandline_args = _parse_args()
     if(commandline_args.dto_file):
         commandline_args = _parse_input_config(commandline_args)
