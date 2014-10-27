702b37c37e1c8b006a09030bd000ebdf0f82621d
Jason Sahl <jasonsahl@gmail.com>
2014-10-27 15:31:36 -0700

    changed function, added action argument

--- scripts/filter_matrix_by_coord.py
+++ scripts/filter_matrix_by_coord.py
@@ -1,41 +1,24 @@
 #!/usr/bin/env python
 
-"""filter matrix to only include a list of known SNPs"""
+"""filter matrix based on a list of known SNPs"""
 
 from __future__ import print_function
 from optparse import OptionParser
 import fileinput
 import itertools
-import sys
-try:
-    import collections
-except:
-    print("you need Python 2.7+ or Python 3+ to run")
-    sys.exit()
+import collections
 
-def test_file(option, opt_str, value, parser):
-    try:
-        with open(value): setattr(parser.values, option.dest, value)
-    except IOError:
-        print('%s cannot be opened' % option)
-        sys.exit()
-   
 def get_index_range(in_matrix):
     matrix=open(in_matrix, "rU")
     firstLine = matrix.readline()
     first_fields = firstLine.split("\t")
-    try:
-        last=first_fields.index("#SNPcall")
-    except:
-        print("matrix is not readable, is this a valid NASP matrix?")
-        sys.exit()
-    matrix.close()
+    last=first_fields.index("#SNPcall")
     return last
 
-def filter_matrix(in_matrix, prefix, coords):
+def filter_matrix(in_matrix, prefix, coords, action):
     """filter NASP matrix for a user provided list of genome
     coordinates"""
-    infile=iter(fileinput.input([in_matrix]))
+    infile=open(in_matrix, "U")
     coords_file = open(coords, "r").read().splitlines()
     out_file=open("%s.coord_filtered.matrix" % prefix, "w")
     firstLine = infile.readline()
@@ -44,12 +27,17 @@ def filter_matrix(in_matrix, prefix, coords):
     lines = [ ]
     for line in infile:
         fields=line.split("\t")
-        if fields[0] in coords_file:
-            lines.append(line)
-            out_file.write(line,)
-    print("Number of provided SNPs:", len(coords_file))
-    print("Number of matching SNPs:", len(lines))
-    out_file.close()
+        if action == "keep":
+            if fields[0] in coords_file:
+                lines.append(line)
+                out_file.write(line,)
+        elif action == "remove":
+            if fields[0] not in coords_file:
+                #lines.append(line)
+                out_file.write(line,)
+            else:
+                lines.append(line)
+    print("Number of SNPs matching in your list:", len(lines))
 
 def matrix_to_fasta(prefix, kind, type, last):
     """converts a NASP matrix to fasta format"""
@@ -63,9 +51,9 @@ def matrix_to_fasta(prefix, kind, type, last):
     for x in test:
         out_fasta.write(">"+str(x[0])+"\n")
         out_fasta.write("".join(x[1:])+"\n")
-    return last
     in_matrix.close()
     out_fasta.close()
+    return last
 
 def filter_missing_matrix(prefix, last):
     """filter a NASP matrix position if it contains either missing
@@ -77,53 +65,54 @@ def filter_missing_matrix(prefix, last):
     lines = [ ]
     for line in matrix:
         fields = line.split("\t")
-        counter=collections.Counter(fields[1:last])
-        values=counter.values()
-        sorted(values,key=int)
-    if "X" not in fields[1:last] and "N" not in fields[1:last] and len(values)>1: file_out.write(line,)
-    if "X" not in fields[1:last] and "N" not in fields[1:last] and len(values)>1: lines.append(line)
-    print("number of filtered SNPs:", len(lines))
+        if "X" not in fields[1:last] and "N" not in fields[1:last]: file_out.write(line,)
+        if "X" not in fields[1:last] and "N" not in fields[1:last]: lines.append(line)
+    print("number of SNPs after missing regions removed:", len(lines))
     matrix.close()
     file_out.close()
-	
+
 def filter_singletons(prefix, matrix_prefix, last):
     """identify the number of autapomorphic SNPs in an ISG matrix"""
     matrix=open("%s.%s" % (prefix, matrix_prefix))
     firstLine = matrix.readline()
+    next(matrix)
     lines = [ ]
     for line in matrix:
-        fields=line.split("\t")
-        counter=collections.Counter(fields[2:last])
+	fields=line.split("\t")
+	counter=collections.Counter(fields[2:last])
         values=counter.values()
-        new_values=list(sorted(values, key=int))
-    for i in range(2,5):
-	    if len(new_values)==int(i) and new_values[int(i-2)]!=1: lines.append(line)
+        values.sort(key=int)
+	for i in range(2,5):
+		if len(values)==int(i) and values[int(i-2)]!=1: lines.append(line)
     print("number of parsimony-informative SNPs:", len(lines))
     matrix.close()
 
-def main(in_matrix, prefix, coords):
+def main(in_matrix, prefix, coords, action):
     last=get_index_range(in_matrix)
-    filter_matrix(in_matrix, prefix, coords)
+    filter_matrix(in_matrix, prefix, coords, action)
     matrix_to_fasta(prefix, "coord_filtered", "raw", last)
     filter_missing_matrix(prefix, last)
     matrix_to_fasta(prefix, "coord_missing_filtered", "filtered", last)
     filter_singletons(prefix, "coord_missing_filtered.matrix", last)
-	
+
 if __name__ == "__main__":
     usage="usage: %prog [options]"
-    parser = OptionParser(usage=usage) 
-    parser.add_option("-m", "--in_matrix", dest="in_matrix",
+    parser = OptionParser(usage=usage)
+    parser.add_option("-i", "--in_matrix", dest="in_matrix",
                       help="/path/to/file [REQUIRED]",
-                      action="callback", callback=test_file, type="string")
+                      action="store", type="string")
     parser.add_option("-p", "--out_prefix", dest="prefix",
                       help="/path/to/file [REQUIRED]",
                       action="store", type="string")
     parser.add_option("-c", "--in_coords", dest="coords",
                       help="/path/to/file [REQUIRED]",
-                      action="callback", callback=test_file, type="string")
+                      action="store", type="string")
+    parser.add_option("-a", "--action", dest="action",
+                      help="action to perform: keep SNPs in range or remove?, defaults to keep",
+                      action="store", type="string", default="keep")
 
     options, args = parser.parse_args()
-    
+
     mandatories = ["in_matrix", "prefix", "coords"]
     for m in mandatories:
         if not options.__dict__[m]:
@@ -131,4 +120,5 @@ if __name__ == "__main__":
             parser.print_help()
             exit(-1)
 
-    main(options.in_matrix,options.prefix,options.coords)
+    main(options.in_matrix,options.prefix,options.coords,options.action)
+
