f7c2430d0012ffcd61315482716d2e999a7ffaa9
Jason Travis <jtravis@tgen.org>
2015-02-10 11:27:54 -0700

    Refactor matrix_dto.py to also index reference in parallel.

--- nasp2/matrix_DTO.py
+++ nasp2/matrix_DTO.py
@@ -15,7 +15,7 @@ from concurrent.futures import ProcessPoolExecutor
 from collections import namedtuple
 from nasp2.parse import Vcf, Fasta
 
-MatrixParameters = namedtuple('MatrixParameters', ['reference_fasta', 'reference_dups', 'minimum_coverage', 'minimum_proportion', 'matrix_folder', 'stats_folder', 'filter_matrix_format'])
+MatrixParameters = namedtuple('MatrixParameters', ['reference_fasta', 'reference_dups', 'minimum_coverage', 'minimum_proportion', 'matrix_folder', 'stats_folder', 'parameters', 'filter_matrix_format'])
 MatrixParameters.__new__.__defaults__ = ("", "", "", "", "", "", "")
 
 
@@ -32,27 +32,6 @@ def _add_input_file(node, filetype, attributes, file):
     return subnode
 
 
-def _parse_files(node):
-    """
-    Returns a tuple of Vcf and Fasta objects indexed in parallel.
-
-    Args:
-        node:
-
-    Return:
-        tuple of Sample:
-    """
-    with ProcessPoolExecutor() as executor:
-        futures = []
-        for frankenfasta in node.iter("frankenfasta"):
-            futures.append(executor.submit(Fasta, frankenfasta.text, frankenfasta.get("name"), frankenfasta.get("aligner")))
-            # input_files.append("%s,%s,::%s" % ("frankenfasta", frankenfasta.get("aligner"), frankenfasta.text))
-        for vcf in node.iter("vcf"):
-            futures.append(executor.submit(Vcf, vcf.text, vcf.get("name"), vcf.get("aligner"), vcf.get("snpcaller")))
-            # input_files.append("%s,%s,%s,::%s" % ("vcf", vcf.get("aligner"), vcf.get("snpcaller"), vcf.text))
-        return tuple(future.result() for future in futures)
-
-
 def write_dto(matrix_parms, franken_fastas, vcf_files, xml_file):
     from xml.dom import minidom
 
@@ -60,6 +39,7 @@ def write_dto(matrix_parms, franken_fastas, vcf_files, xml_file):
     parm_node = ElementTree.SubElement(root, "parameters")
     _write_parameters(parm_node, matrix_parms)
     files_node = ElementTree.SubElement(root, "files")
+    # TODO: Can 'name' be redefined to be just the sample name instead of the filename without the extension?
     for (name, aligner, file) in franken_fastas:
         attributes = {'name': name, 'aligner': aligner}
         _add_input_file(files_node, "frankenfasta", attributes, file)
@@ -74,11 +54,36 @@ def write_dto(matrix_parms, franken_fastas, vcf_files, xml_file):
 
 
 def parse_dto(xml_file):
+    """
+    Args:
+        xml_file:
+
+    Returns:
+        MatrixParameters, tuple of Analyses
+    """
     xmltree = ElementTree.parse(xml_file)
     root = xmltree.getroot()
-    matrix_parms = MatrixParameters({element.tag: element.text for element in root.find("parameters").iter()})
-    input_files = _parse_files(root.find("files"))
-    return matrix_parms, input_files
+
+    futures = []
+    with ProcessPoolExecutor() as executor:
+        # Parse matrix parameters indexing the reference and duplicates file in parallel.
+        matrix_params = {element.tag.replace('-', '_'): element.text for element in root.find("parameters").iter()}
+        matrix_params['reference_fasta'] = executor.submit(Fasta, matrix_params['reference_fasta'], 'reference', None)
+        # TODO: handle undefined reference_dups
+        matrix_params['reference_dups'] = executor.submit(Fasta, matrix_params['reference_dups'], 'reference', None)
+        matrix_params['minimum_coverage'] = float(matrix_params['minimum_coverage'])
+        matrix_params['minimum_proportion'] = float(matrix_params['minimum_proportion'])
+
+        # Index Vcf and Frankenfastas in parallel.
+        for frankenfasta in root.find("files").iter("frankenfasta"):
+            futures.append(executor.submit(Fasta, frankenfasta.text, frankenfasta.get("name"), frankenfasta.get("aligner")))
+        for vcf in root.find("files").iter("vcf"):
+            futures.append(executor.submit(Vcf, vcf.text, vcf.get("name"), vcf.get("aligner"), vcf.get("snpcaller")))
+
+        # Return when the indexing processes complete.
+        matrix_params['reference_fasta'] = matrix_params['reference_fasta'].result()
+        matrix_params['reference_dups'] = matrix_params['reference_dups'].result()
+        return MatrixParameters(**matrix_params), tuple(future.result() for future in futures)
 
 
 def main():
