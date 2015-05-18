b8395cadeb0301550158634fa22f74f71049fbf4
Jason Travis <jtravis@tgen.org>
2015-05-18 13:39:22 -0700

    Add GenomeCollection test

--- tests/nasp/nasp_objects/genome_collection_test.py
+++ tests/nasp/nasp_objects/genome_collection_test.py
@@ -2,9 +2,34 @@
 
 __author__ = 'jtravis'
 
+import tempfile
 import unittest
-from nasp.nasp_objects import GenomeCollection
+from nasp.nasp_objects import ReferenceGenome, FastaGenome, GenomeCollection
+
+from tests import testdata
 
 class GenomeCollectionTestCase(unittest.TestCase):
     def setUp(self):
+        reference_path = testdata.REFERENCE_FASTA
+        dups_path = testdata.REFERENCE_DUPS
+        reference = ReferenceGenome()
+        reference.import_fasta_file(reference_path)
+        reference.import_dups_file(dups_path)
         self.genome = GenomeCollection()
+        fasta = FastaGenome()
+        self.genome.add_genome(fasta)
+        self.genome.set_reference(reference)
+        self.matrix_formats = [
+            {
+                'dataformat': 'matrix',
+                'handle': tempfile.TemporaryFile(mode='w'),
+                'filter': ''
+            }
+        ]
+
+    def tearDown(self):
+        for matrix in self.matrix_formats:
+            matrix['handle'].close()
+
+    def test_send_to_matrix_handles(self):
+        self.genome.send_to_matrix_handles(self.matrix_formats)
