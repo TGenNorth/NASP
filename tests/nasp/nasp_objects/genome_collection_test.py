a52619e6027c50cd66ec83898e52c4ed6b2cc02b
Jason Travis <jtravis@tgen.org>
2015-05-22 09:26:55 -0700

    genome_collection: write_stats_data test

--- tests/nasp/nasp_objects/genome_collection_test.py
+++ tests/nasp/nasp_objects/genome_collection_test.py
@@ -10,23 +10,40 @@ from nasp.nasp_objects import ReferenceGenome, FastaGenome, GenomeCollection
 from tests import testdata
 
 class GenomeCollectionStatsTestCase(unittest.TestCase):
+    maxDiff = None
+
     def setUp(self):
+        # reference_path = testdata.REFERENCE_FASTA
+        # dups_path = testdata.REFERENCE_DUPS
+        # reference = ReferenceGenome()
+        # reference.import_fasta_file(reference_path)
+        # reference.import_dups_file(dups_path)
+        # self.genome = GenomeCollection()
+        # self.genome.set_reference(reference)
+        # fasta = FastaGenome()
+        # fasta.import_fasta_file(reference_path)
+        # fasta.set_file_path(reference_path)
+        # fasta.set_file_type('fasta')
+        # fasta.set_nickname('test_nickname')
+        # fasta.add_generators(['genA', 'genB'])
+        # self.genome.add_genome(fasta)
+
         reference_path = testdata.REFERENCE_FASTA
         dups_path = testdata.REFERENCE_DUPS
         reference = ReferenceGenome()
         reference.import_fasta_file(reference_path)
         reference.import_dups_file(dups_path)
         self.genome = GenomeCollection()
-        self.genome.set_reference(reference)
         fasta = FastaGenome()
         fasta.import_fasta_file(reference_path)
-        fasta.set_file_path(reference_path)
-        fasta.set_file_type('fasta')
-        fasta.set_nickname('test_nickname')
-        fasta.add_generators(['genA', 'genB'])
         self.genome.add_genome(fasta)
+        self.genome.set_reference(reference)
+        # Statistics are gathered when the matrices are created
+        self.genome.write_to_matrices({})
 
         self.tmpfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        #
+        # self.tmpfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
 
         # self.matrix_formats = [
         #     {
@@ -41,7 +58,12 @@ class GenomeCollectionStatsTestCase(unittest.TestCase):
 
     def test_send_to_matrix_handles(self):
         from vcf_to_matrix import write_stats_data
-        expected = ''
+        expected = (
+            "Contig	reference_length	reference_clean	reference_clean (%)	reference_duplicated	reference_duplicated (%)	all_called	all_called (%)	all_passed_coverage	all_passed_coverage (%)	all_passed_proportion	all_passed_proportion (%)	all_passed_consensus	all_passed_consensus (%)	quality_breadth	quality_breadth (%)	any_snps	any_snps (%)	best_snps	best_snps (%)	\n"
+	        "	stat descriptions go here\n"
+            "Whole Genome	3977	3977	100.00%	0	0.00%	3977	100.00%	3977	100.00%	3977	100.00%	3977	100.00%	3977	100.00%	0	0.00%	0	0.00%	\n"
+            "500WT1_test	3977	3977	100.00%	0	0.00%	3977	100.00%	3977	100.00%	3977	100.00%	3977	100.00%	3977	100.00%	0	0.00%	0	0.00%	\n"
+        )
 
         with tempfile.TemporaryDirectory() as tmpdir:
             write_stats_data(self.genome, tmpdir)
@@ -49,8 +71,7 @@ class GenomeCollectionStatsTestCase(unittest.TestCase):
             os.listdir(tmpdir)
 
             with open(tmpdir + '/general_stats.tsv') as handle:
-                for line in handle:
-                    print(line, end='')
+                self.assertEqual(expected, ''.join(handle.readlines()))
 
 class GenomeCollectionMasterMatrixFastaTestCase(unittest.TestCase):
     def setUp(self):
@@ -80,15 +101,18 @@ class GenomeCollectionMasterMatrixFastaTestCase(unittest.TestCase):
 
     def test_send_to_matrix_handles(self):
         expected = """LocusID	Reference	None	#SNPcall	#Indelcall	#Refcall	#CallWasMade	#PassedDepthFilter	#PassedProportionFilter	#A	#C	#G	#T	#Indel	#NXdegen	Contig	Position	InDupRegion	SampleConsensus	CallWasMade	PassedDepthFilter	PassedProportionFilter	Pattern	Pattern#
-500WT1_test::1	A	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	1	False	False	'1N'	1
-500WT1_test::2	G	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	2	False	False	'1N'	1
-500WT1_test::3	C	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	3	False	False	'1N'	1
-500WT1_test::4	G	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	4	False	False	'1N'	1
-500WT1_test::5	C	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	5	False	False	'1N'	1
-500WT1_test::6	C	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	6	False	False	'1N'	1
-500WT1_test::7	C	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	7	False	False	'1N'	1
-500WT1_test::8	A	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	8	False	False	'1N'	1
-500WT1_test::9	A	0	0	0	0/1	1/1	1/1	0	0	0	0	0	1	500WT1_test	9	False	False	'1N'	1"""
+500WT1_test::1	A	0	0	1	1/1	1/1	1/1	1	0	0	0	0	0	500WT1_test	1	False	True	'11'	1
+500WT1_test::2	G	0	0	1	1/1	1/1	1/1	0	0	1	0	0	0	500WT1_test	2	False	True	'11'	1
+500WT1_test::3	C	0	0	1	1/1	1/1	1/1	0	1	0	0	0	0	500WT1_test	3	False	True	'11'	1
+500WT1_test::4	G	0	0	1	1/1	1/1	1/1	0	0	1	0	0	0	500WT1_test	4	False	True	'11'	1
+500WT1_test::5	C	0	0	1	1/1	1/1	1/1	0	1	0	0	0	0	500WT1_test	5	False	True	'11'	1
+500WT1_test::6	C	0	0	1	1/1	1/1	1/1	0	1	0	0	0	0	500WT1_test	6	False	True	'11'	1
+500WT1_test::7	C	0	0	1	1/1	1/1	1/1	0	1	0	0	0	0	500WT1_test	7	False	True	'11'	1
+500WT1_test::8	A	0	0	1	1/1	1/1	1/1	1	0	0	0	0	0	500WT1_test	8	False	True	'11'	1
+500WT1_test::9	A	0	0	1	1/1	1/1	1/1	1	0	0	0	0	0	500WT1_test	9	False	True	'11'	1
+500WT1_test::10	T	0	0	1	1/1	1/1	1/1	0	0	0	1	0	0	500WT1_test	10	False	True	'11'	1"""
+        # ...
+        # There might be more data, but 10 samples is enough
 
         self.genome.send_to_matrix_handles(self.matrix_formats)
 
