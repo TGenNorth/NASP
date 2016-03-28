7258360a5e816b54bd8dc716fc98dba55b1f9ea8
Jason Sahl <jasonsahl@gmail.com>
2016-03-28 12:56:50 -0700

    eased functionality

--- scripts/annotate_NASP.py
+++ scripts/annotate_NASP.py
@@ -79,10 +79,60 @@ def parse_output_file():
         outfile.write("\t".join(alist)+"\n")
     outfile.close()
 
+def find_reference_genome(reference, snp_eff):
+    os.system("java -jar %s databases | grep %s > tmp.snpeff.out" % (snp_eff,reference))
+    num_lines = sum(1 for line in open('tmp.snpeff.out'))
+    if num_lines == 0:
+        print("no hits found for your search criteria, try again with the strain name")
+        sys.exit()
+    elif num_lines > 1:
+        print("multiple hits found for your search criteria, refine search criteria")
+        os.system("cat tmp.snpeff.out")
+        sys.exit()
+    else:
+        for line in open("tmp.snpeff.out"):
+            fields = line.split()
+            reference_id = fields[0]
+    os.system("rm tmp.snpeff.out")
+    return reference_id
+
+def make_map_file(ref_id, snp_eff, vcf):
+    os.system("java -jar %s eff -v -download -no-downstream -no-upstream %s %s > tmp.snpeff.out 2>&1" % (snp_eff,ref_id,vcf))
+    with open("tmp.snpeff.out") as myFile:
+        for num, line in enumerate(myFile, 1):
+            if "# Number of chromosomes" in line:
+                fields = line.split()
+                num_chromes = int(fields[-1])
+                ref_line = num
+    line_end = num_chromes+ref_line
+    print("")
+    print("create mapping file with the following information:")
+    with open("tmp.snpeff.out") as myFile:
+        for line in myFile.readlines()[ref_line:line_end+1]:
+            print(line)
+    chromosomes = []
+    for line in open(vcf):
+        fields = line.split()
+        if line.startswith("#"):
+            pass
+        else:
+            chromosomes.append(fields[0].split("::"))
+    nr=[x for i, x in enumerate(chromosomes) if x not in chromosomes[i+1:]]
+    print("")
+    print("Your chromosome names are:")
+    for chromosome in nr:
+        print("".join(chromosome))
+    os.system("rm tmp.snpeff.out")
+    sys.exit()
+
 def main(in_matrix, vcf, snp_eff, map, reference):
-    my_map = make_map_dict(map)
+    ref_id = find_reference_genome(reference, snp_eff)
+    if "NULL" in map:
+        make_map_file(ref_id, snp_eff, vcf)
+    else:
+        my_map = make_map_dict(map)
     rename_vcf_file(vcf, my_map)
-    run_snpeff(snp_eff, reference)
+    run_snpeff(snp_eff, ref_id)
     parse_output_file()
     """removes the white space lines"""
     os.system("awk '$NF>0' tmp.matrix.xyx > pruned.matrix.xyx")
@@ -105,10 +155,10 @@ if __name__ == "__main__":
                       help="name of snpEff reference [REQUIRED]",
                       action="store", type="string")
     parser.add_option("-m", "--map", dest="map",
-                      help="/path/to/mapping file [REQUIRED]",
-                      action="callback", callback=test_file, type="string")
+                      help="/path/to/mapping file",
+                      action="callback", callback=test_file, default="NULL", type="string")
     options, args = parser.parse_args()
-    mandatories = ["in_matrix", "vcf", "snp_eff", "reference", "map"]
+    mandatories = ["in_matrix", "vcf", "snp_eff", "reference"]
     for m in mandatories:
         if not options.__dict__[m]:
             print("\nMust provide %s.\n" %m)
@@ -116,4 +166,3 @@ if __name__ == "__main__":
             exit(-1)
 
     main(options.in_matrix,options.vcf,options.snp_eff,options.map,options.reference)
-
