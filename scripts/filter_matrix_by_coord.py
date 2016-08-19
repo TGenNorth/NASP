6543b4f1461667c2366c374349d9c5c3f90e09bb
Jason Sahl <jasonsahl@gmail.com>
2016-08-19 14:53:39 -0700

    script now python3 compatible

--- scripts/filter_matrix_by_coord.py
+++ scripts/filter_matrix_by_coord.py
@@ -85,11 +85,11 @@ def filter_singletons(prefix, matrix_prefix, last):
         counter=collections.Counter(fields[1:last])
         values=counter.values()
         new_values=list(sorted(values, key=int))
-        if len(values)==1: sys.exc_clear()
+        if len(new_values)==1: sys.exc_clear()
         else:
             for i in range(2,5):
-                if len(values)==int(i) and values[i-2]>1: file_out.write(line,)
-                if len(values)==int(i) and values[i-2]>1: lines.append(line)
+                if len(values)==int(i) and new_values[i-2]>1: file_out.write(line,)
+                if len(values)==int(i) and new_values[i-2]>1: lines.append(line)
         values=[]
     print("number of parsimony-informative SNPs:", len(lines))
     matrix.close()
